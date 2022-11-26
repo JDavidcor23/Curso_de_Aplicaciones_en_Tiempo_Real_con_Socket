ac903d4b297f6c3fb83e0736b6705c8a4917a039
jorgedaviddiaz <jorgedavid23diaz@gmail.com>
2022-11-26 17:23:22 -0500

    clase 9*

--- src/index.js
+++ src/index.js
@@ -1,35 +1,30 @@
 const express = require("express");
+const path = require("path");
 const { createServer } = require("http");
 const { Server } = require("socket.io");
-const path = require("path");
-//SERVER
+
 const app = express();
 const httpServer = createServer(app);
 const io = new Server(httpServer);
 
-/* TODOS LOS ARCHIVOS QUE VA A LEER NODE Y SE LO VA A 
-ENVIAR AL CLIENTE CUANDO HABRA EL LOCALHOST */
+app.use( express.static(path.join(__dirname, "views")) );
 
-app.use(express.static(path.join(__dirname, "/views")));
-
-/* VISTA HTML localhost:3000 */
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/views/index.html");
-});
-io.on("connection", (socket) => {
-  // CUANTOS CLIENTES TENGO
-  console.log("Clientes conectados: ", socket.engine.clientsCount);
-
-  // CUANDO EL CLIENTE SE DESCONECTA
-  socket.on("disconnect", () => {
-    console.log("El socket se ha desconactado");
-  });
-
-  // DETECTA CADA VEZ QUE PASAMOS DE HTTP LONG-POLLING A SOCKETIO
-  socket.conn.once("upgrade", () => {
-    console.log("HTTP long-Polling a: ", socket.conn.transport.name);
-  });
+    res.sendFile(__dirname + "/views/index.html");
 });
 
-/* PORT */
+io.on("connection", socket => {
+
+    // Emisión básica
+    socket.emit("welcome", "Ahora estás conectado 😎.");
+
+    socket.on("server", data => {
+        console.log(data);
+    });
+
+    // Emisión a todos
+    io.emit("everyone", socket.id + " se ha conectado 👀");
+
+});
+
 httpServer.listen(3000);
