def75d23dd1aae1a9ec076a8d5ba7f9d5ef84a5d
jorgedaviddiaz <jorgedavid23diaz@gmail.com>
2022-11-28 08:05:08 -0500

    clase-13

--- src/index.js
+++ src/index.js
@@ -13,49 +13,23 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
 });
 
-io.on("connection", (socket) => {
-  //SOCKET NOS PERMITE CREAR NUESTROS PROPIOS METODOS
-  //ESTA PROPIEDAD CUSTOM NOS AYUDA A SABER EN QUE SALA ESTA CONECTADO
-  socket.connectedRoom = "";
+//CUANDO HACEMOS ESTO EL SOCKET SE CONECTA DE MANERA AUTOMATICA 'DEFAULT'
 
-  //RECIBO LA SALA
-  socket.on("connect to room", (room) => {
-    //DESCONECTA DE LA SALA ANTERIOR AL USUARIO
-    socket.leave(socket.connectedRoom);
+/* io.on("connection", socket => {
+}); */
 
-    switch (room) {
-      //QUE SALA ME QUIERO CONECTAR
-      case "room1":
-        //METODO JOIN METE AL EL SOCKET QUE ESTA ENVIANDO A X SALE EN ESTE CASO ROOM1
-        //SI LA SALA NO EXISTE LO VA A CREAR AUTOMATICAMENTE
-        socket.join("room1");
-        //MODIFICAMOS LA VARIABLE PARA INDICAR EN QUE SALA ESTOY
-        socket.connectedRoom = "room1";
-        break;
+//CREAMOS LOS NAMESPACES
+const teachers = io.of("teachers");
+const students = io.of("students");
 
-      case "room2":
-        socket.join("room2");
-        socket.connectedRoom = "room2";
-        break;
+//EMPEZAMOS A ESCUCHAR
+teachers.on("connection", (socket) => {
+  console.log(socket.id + " se ha conectado a la sala de profes");
+});
 
-      case "room3":
-        socket.join("room3");
-        socket.connectedRoom = "room3";
-        break;
-    }
-  });
-
-  // OBTENGO EL MENSAJE DEL FORNT
-  socket.on("message", (message) => {
-    // OBTENGO LA SALA
-    const room = socket.connectedRoom;
-
-    // ENVIO EL MENSAJE A LA SALA
-    io.to(room).emit("send message", {
-      message,
-      room,
-    });
-  });
+//EMPEZAMOS A ESCUCHAR
+students.on("connection", (socket) => {
+  console.log(socket.id + " se ha conectado a la sala de estudiantes");
 });
 
 httpServer.listen(3000);
--- src/views/js/index.js
+++ src/views/js/index.js
@@ -1,42 +1,31 @@
-const socket = io();
+//CUANDO HACEMOS ESTO EL SOCKET SE CONECTA DE MANERA AUTOMATICA 'DEFAULT'
+// const socket = io();
 
-// Selecciono mis 3 botones que me permitirán conectarme a las salas
-const connectRoom1 = document.querySelector("#connectRoom1");
-const connectRoom2 = document.querySelector("#connectRoom2");
-const connectRoom3 = document.querySelector("#connectRoom3");
+//QUE GRUPO PERTENECEMOS
+const user = prompt("Escribe tu usuario");
 
-// Eventos para que al hacer click me conecte a las salas
+//LISTA DE PROFES
+const profes = ["RetaxMaster", "juandc", "GNDX"];
 
-connectRoom1.addEventListener("click", () => {
-  socket.emit("connect to room", "room1");
-});
+//VARIABLES
+let socketNamespace, group;
 
-connectRoom2.addEventListener("click", () => {
-  socket.emit("connect to room", "room2");
-});
+const chat = document.querySelector("#chat");
+const namespace = document.querySelector("#namespace");
 
-connectRoom3.addEventListener("click", () => {
-  socket.emit("connect to room", "room3");
-});
+//SI EL USER ESTA INCLUIDO EN LA LISTA DE PROFES
+if (profes.includes(user)) {
+  //NOS CONECTAMOS AL NAMESPACE DE TEACHERS
+  socketNamespace = io("/teachers");
+  group = "teachers";
+} else {
+  //SI NO AL DE ESTUDIANTES
+  socketNamespace = io("/students");
+  group = "students";
+}
 
-// Enviar mensaje
-
-const sendMessage = document.querySelector("#sendMessage");
-
-sendMessage.addEventListener("click", () => {
-  const message = prompt("Escribe tu mensaje:");
-
-  socket.emit("message", message);
-});
-
-// Recibir el evento del mensaje DEL BACK
-
-socket.on("send message", (data) => {
-  const { room } = data;
-  const { message } = data;
-
-  const li = document.createElement("li");
-  li.textContent = message;
-
-  document.querySelector(`#${room}`).append(li);
+//CUANDO YA DECLAREMOS EL SOCKET NOS CONECTAMOS Y ESCUCHAMOS
+socketNamespace.on("connect", () => {
+  //ESCRIBIMOS EN EL HTML
+  namespace.textContent = group;
 });
